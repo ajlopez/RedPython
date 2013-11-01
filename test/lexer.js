81adf573dcc42565b14979faeb742af7c0a56bd8
ajlopez <webmaster@ajlopez.com>
2013-11-01 13:55:09 -0300

    Lexer first test, WIP

---
+++ test/lexer.js
@@ -0,0 +1,19 @@
+
+var lexer = require('../lib/lexer.js');
+TokenType = lexer.TokenType;
+
+exports['get indent and name'] = function (test) {
+    var lxr = lexer.createLexer('name');
+    
+    var token = lxr.nextToken();
+    test.ok(token);
+    test.equal(token.type, TokenType.Indent);
+    test.equal(token.value, 0);
+    
+    token = lxr.nextToken();
+    test.ok(token);
+    test.equal(token.type, TokenType.Name);
+    test.equal(token.value, 'name');
+    
+    test.strictEqual(lxr.nextToken(), null);
+};
