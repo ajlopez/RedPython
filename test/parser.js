889c4308922b7da2bd2c063ceb62d8173eefacb3
ajlopez <webmaster@ajlopez.com>
2013-11-01 18:30:04 -0300

    Parse indent with zero size

--- test/parser.js
+++ test/parser.js
@@ -3,11 +3,18 @@ var parser = require('../lib/parser.js');
 
 exports['compile integer'] = function (test) {
     var parsr = parser.createParser('123');
-    parsr.parseIndent(0);
+    parsr.parseIndent();
     
     var expr = parsr.parseExpression();
     test.ok(expr);
     test.equal(expr.compile(), '123');
     
     test.strictEqual(parsr.parseExpression(), null);
+};
+
+exports['get indent with size zero'] = function (test) {
+    var parsr = parser.createParser('name');
+    var indent = parsr.parseIndent();
+    
+    test.strictEqual(indent, 0);
 };
